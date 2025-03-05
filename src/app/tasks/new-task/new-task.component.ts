97e0b700102c78e7506c216b4b98ba59eb4ad4f4
vighnesh-kargutkar <kargutkar860@gmail.com>
2025-03-05 14:00:22 +0530

    created and using a service

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,8 +11,13 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  private taskService: TasksService;
+  constructor(){
+    this.taskService = new TasksService();
+  }
 
   onAddTask(title: string, description: string) {
+    this.taskService.addTask({ title, description });
     this.formEl()?.nativeElement.reset();
   }
 }
